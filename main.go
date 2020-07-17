d13b5c335f2e8771caa2d4eb663de2442ceaefbf
Justin Kolberg <amd.prophet@gmail.com>
2020-07-16 18:52:08 -0700

    refactor main.go
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- main.go
+++ main.go
@@ -10,23 +10,23 @@ import (
 	"github.com/amdprophet/packagecloud-go/packagecloud"
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
+)
+
+const (
+	flagConfig  = "config"
+	flagURL     = "url"
+	flagToken   = "token"
+	flagVerbose = "verbose"
+
+	defaultURL     = "https://packagecloud.io"
+	defaultToken   = ""
+	defaultVerbose = false
 )
 
 var (
 	cfgFile string
-	url     string
-	token   string
-	verbose bool
-
-	rootCmd = &cobra.Command{
-		Use:   "packagecloud",
-		Short: "A Go alternative to the official packagecloud command-line client",
-		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-
-			return nil
-		},
-	}
 )
 
 func er(msg interface{}) {
@@ -34,56 +34,91 @@ func er(msg interface{}) {
 	os.Exit(1)
 }
 
-func init() {
-	cobra.OnInitialize(initConfig)
+func defaultConfigPath() (string, error) {
+	home, err := homedir.Dir()
+	if err != nil {
+		return "", err
+	}
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.packagecloud)")
-	rootCmd.PersistentFlags().StringVar(&url, "url", "", "website url to use (default is https://packagecloud.io)")
-	rootCmd.PersistentFlags().StringVar(&token, "token", "", "token to use for authentication")
-	rootCmd.PersistentFlags().BoolVar(&verbose, "verbose", false, "enable verbose mode")
-
-	viper.BindPFlag("url", rootCmd.PersistentFlags().Lookup("url"))
-	viper.BindPFlag("token", rootCmd.PersistentFlags().Lookup("token"))
-	viper.BindPFlag("verbose", rootCmd.PersistentFlags().Lookup("verbose"))
+	return filepath.Join(home, ".packagecloud"), nil
 }
 
-func initConfig() {
-	viper.SetConfigType("json")
+func initCobra(cmd *cobra.Command) func() {
+	return func() {
+		viper.SetEnvPrefix("packagecloud")
+		viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+		viper.AutomaticEnv()
 
-	if cfgFile != "" {
-		fmt.Println("set")
-		viper.SetConfigFile(cfgFile)
-	} else {
-		home, err := homedir.Dir()
-		if err != nil {
+		if err := postInitCommands(cmd.Commands()); err != nil {
 			er(err)
 		}
 
-		viper.SetConfigType("yaml")
-		viper.SetConfigFile(filepath.Join(home, ".packagecloud"))
-	}
+		viper.SetConfigType("json")
+		viper.SetConfigFile(cfgFile)
 
-	viper.SetEnvPrefix("packagecloud")
-	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
-	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err != nil {
-		er(fmt.Errorf("error reading config: %s", err))
+		if err := viper.ReadInConfig(); err != nil {
+			er(fmt.Errorf("error reading config: %s", err))
+		}
 	}
 }
 
-func main() {
-	rootCmd, err := newRootCmd()
-	if err != nil {
-		er(err)
-	}
-	cobra.OnInitialize(initCobra(rootCmd))
+func postInitCommands(commands []*cobra.Command) error {
+	for _, cmd := range commands {
+		if err := presetRequiredFlags(cmd); err != nil {
+			return err
+		}
 
-	if err := rootCmd.Execute(); err != nil {
-		er(err)
+		if cmd.HasSubCommands() {
+			if err := postInitCommands(cmd.Commands()); err != nil {
+				return err
+			}
+		}
 	}
+	return nil
+}
+
+func presetRequiredFlags(cmd *cobra.Command) error {
+	if err := viper.BindPFlags(cmd.Flags()); err != nil {
+		return err
+	}
+
+	var fErr error
+	cmd.Flags().VisitAll(func(f *pflag.Flag) {
+		if viper.IsSet(f.Name) && viper.GetString(f.Name) != "" {
+			if err := cmd.Flags().Set(f.Name, viper.GetString(f.Name)); err != nil {
+				fErr = err
+				return
+			}
+		}
+	})
+	return fErr
+}
+
+func newRootCmd() (*cobra.Command, error) {
+	cmd := &cobra.Command{
+		Use:   "packagecloud",
+		Short: "A Go alternative to the official packagecloud command-line client",
+	}
+
+	defaultConfig, err := defaultConfigPath()
+	if err != nil {
+		return nil, err
+	}
+
+	cmd.PersistentFlags().StringVar(&cfgFile, flagConfig, defaultConfig, "config file")
+	cmd.PersistentFlags().String(flagURL, defaultURL, "website url to use")
+	cmd.PersistentFlags().String(flagToken, defaultToken, "token to use for authentication")
+	cmd.PersistentFlags().Bool(flagVerbose, defaultVerbose, "enable verbose mode")
+
+	cmd.MarkFlagRequired(flagToken)
+
+	viper.BindPFlag(flagURL, cmd.PersistentFlags().Lookup(flagURL))
+	viper.BindPFlag(flagToken, cmd.PersistentFlags().Lookup(flagToken))
+	viper.BindPFlag(flagVerbose, cmd.PersistentFlags().Lookup(flagVerbose))
+
 	getClientFn := func() (*packagecloud.Client, error) {
 		var config packagecloud.Config
+
 		if err := viper.Unmarshal(&config); err != nil {
 			return nil, fmt.Errorf("unable to unmarshal config: %s", err)
 		}
@@ -93,7 +128,18 @@ func main() {
 
 		return packagecloud.NewClient(config), nil
 	}
-	command.AddCommands(rootCmd, getClientFn)
+
+	command.AddCommands(cmd, getClientFn)
+
+	return cmd, nil
+}
+
+func main() {
+	rootCmd, err := newRootCmd()
+	if err != nil {
+		er(err)
+	}
+	cobra.OnInitialize(initCobra(rootCmd))
 
 	if err := rootCmd.Execute(); err != nil {
 		er(err)
